de193a7979d9f148192abf0312d8e4cef58cf136
pppo0o0 <3x3x3yryr@gmail.com>
2020-09-05 21:18:48 +0900

    custom

---
+++ README.md
@@ -0,0 +1,14 @@
+# web制作ベース
+
+## 土台
+https://olein-design.com/blog/build-webpage-by-npm-script
+
+## 起動
+```
+npm run watch
+```
+
+## 含まれているもの
+- fontawesome
+- jQuery
+- AOS
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     plugins: [
         require('autoprefixer')({
-            browsers: ['last 2 versions'],
             cascade: false
         }),
         require('cssnano')({
